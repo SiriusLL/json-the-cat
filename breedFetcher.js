9916a564c6a6516c4f710e5b4f129a8a3373106a
SiriusLL <sirius.love.light@gmail.com>
2021-03-11 22:04:11 +0000

    added conditions for errors or no data returned

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,27 +1,32 @@
-
-const request = require('request');
-const fs = require('fs');
-let url = `https://api.thecatapi.com/v1/breeds/search?q=`
-const breadName = process.argv[2];
+//require request and fs, variable for unquiried "baseUrl" url & user input
+const request = require("request");                                             
+const fs = require("fs");
+let baseUrl = `https://api.thecatapi.com/v1/breeds/search?q=`;
+const userInput = process.argv[2];
 //const fPath = process.argv[3];
 
-
-
-const bread = function(breadName) {
-  url += breadName;
+//function takes in user input of breed and call back to return data to access data
+//variable for url appends breedname onto quiry
+const breed = function (breedName, getBread) {
+  const url = `${baseUrl}${breedName}`;
+  //request url with function that calls errors, responses, and body
   request(url, (error, response, body) => {
+    //parses the body array into a readable array containing and object
     const data = JSON.parse(body);
-    if (!data[0].breadName) {
-      console.log(`${breadName} not found!!!`)
+    //check if there is an error through request param, or if there is no data
+    if (error || !data.length) {
+      console.log(`${breedName} not found!!!`);
       return;
     }
-
-      console.log(data[0].description);
-      //console.log(typeof data);
-    
-
+    //calls the getBread function with param of data object key to retrieve description
+    getBread(data[0].description);
   });
-}
+};
+//is called to get info out of the breed function
+const getBread = function (breed) {
+  console.log(breed);
+  return breed;
+};
 
 // Use the callback based approach we've been learning so far
-bread(breadName);
+console.log(breed(userInput, getBread));
